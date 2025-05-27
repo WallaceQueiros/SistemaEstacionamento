4099660db985fb2d747b3caa86d28274ef01119d
Wallace Queiros <wallacequeiros@hotmail.com>
2025-05-27 10:53:55 -0300

    Criação do menu de opções

--- SistemaEstacionamento/Program.cs
+++ SistemaEstacionamento/Program.cs
@@ -1,4 +1,19 @@
 ﻿
-Console.WriteLine("Olá, Mundo!");
 
+string opcao = "";
 
+while (true)
+{
+    Console.WriteLine("Bem-Vindo ao Sistema de Estacionamento!");
+    Console.WriteLine();
+    Console.WriteLine("Opções: ");
+    Console.WriteLine(" 1 - Cadastrar Veículo\n 2 - Remover Veículo\n 3 - Listar Veículos\n 4 - Sair");
+    Console.Write("Digite sua opção: ");
+    opcao = Console.ReadLine();
+
+    switch (opcao)
+    {
+        case "1":
+            break;
+    }
+}
