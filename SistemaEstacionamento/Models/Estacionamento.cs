9457162c28917c1cc533c17a4f6861943bc2b051
Wallace Queiros <wallacequeiros@hotmail.com>
2025-05-27 12:01:46 -0300

    feat: adicionar métodos para cadastrar, remover e listar veículos

--- SistemaEstacionamento/Models/Estacionamento.cs
+++ SistemaEstacionamento/Models/Estacionamento.cs
@@ -2,5 +2,21 @@
 
 public class Estacionamento
 {
-    
+    List<string> veiculos = new List<string>();
+    public string CadastrarVeiculo(string placa)
+    {
+        veiculos.Add(placa);
+        return placa;
+    }
+
+    public int RemoverVeiculo(int indice)
+    {
+        veiculos.RemoveAt(indice);
+        return indice;
+    }
+
+    public string ListarVeiculos()
+    {
+        return string.Join(", ", veiculos);
+    }
 }
