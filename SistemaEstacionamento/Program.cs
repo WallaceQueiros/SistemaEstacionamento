48324f52cc71ef7bab0e7b31cd92a9d8ad52015c
Wallace Queiros <wallacequeiros@hotmail.com>
2025-05-27 12:04:29 -0300

    feat: implementação do menu de opções no console para cadastro, remoção e listar de veículos

--- SistemaEstacionamento/Program.cs
+++ SistemaEstacionamento/Program.cs
@@ -1,4 +1,6 @@
-﻿
+﻿using SistemaEstacionamento.Models;
+
+Estacionamento estacionamento = new Estacionamento();
 
 string opcao = "";
 
@@ -14,6 +16,24 @@ while (true)
     switch (opcao)
     {
         case "1":
+            Console.WriteLine("Cadastre a placa do Veiculo: ");
+            estacionamento.CadastrarVeiculo(Console.ReadLine());
+            Console.WriteLine("Cadastro Realizado!");
+            break;
+        
+        case "2":
+            Console.WriteLine("Digite o Veículo que deseja remover: ");
+            estacionamento.RemoverVeiculo(Convert.ToInt32(Console.ReadLine()));
+            Console.WriteLine("Carro removido com sucesso!");
+            break;
+        
+        case "3":
+            Console.WriteLine("Veiculos Cadastrados: ");
+            Console.WriteLine(estacionamento.ListarVeiculos());
+            break;
+        
+        case "4":
+            Console.WriteLine("Saindo do Sistema...");
             break;
     }
 }
--- SistemaEstacionamento/obj/Debug/net9.0/SistemaEstacionamento.AssemblyInfo.cs
+++ SistemaEstacionamento/obj/Debug/net9.0/SistemaEstacionamento.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SistemaEstacionamento")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4099660db985fb2d747b3caa86d28274ef01119d")]
 [assembly: System.Reflection.AssemblyProductAttribute("SistemaEstacionamento")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SistemaEstacionamento")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
